07935d1295cc78474c435662be5c0b39c83e9856
Jorge Cellerino <jorge.X.Cellerino.-ND@email.disney.com>
2015-11-21 19:35:07 -0300

    dropwizard hello world running OK

--- back-end/src/main/java/com/toptal/expenses/HelloWorldApplication.java
+++ back-end/src/main/java/com/toptal/expenses/HelloWorldApplication.java
@@ -1,27 +1,35 @@
 package com.toptal.expenses;
 
+import com.toptal.expenses.resources.HelloWorldResource;
+
+import health.TemplateHealthCheck;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
 
-	public static void main(String[] args) throws Exception {
-		new HelloWorldApplication().run(args);
-	}
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
 
-	@Override
-	public void run(HelloWorldConfiguration configuration,
-			Environment environment) throws Exception {
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+        //adding a resource
+        final HelloWorldResource resource = new HelloWorldResource(configuration.getTemplate(),
+                configuration.getDefaultName());
+        environment.jersey().register(resource);
+        //adding a health check
+        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+    }
 
-	}
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
 
-	@Override
-	public String getName() {
-		return "hello-world";
-	}
-	
-	@Override
+    @Override
     public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
         // nothing to do yet
     }
--- back-end/src/main/java/com/toptal/expenses/resources/HelloWorldResource.java
+++ back-end/src/main/java/com/toptal/expenses/resources/HelloWorldResource.java
@@ -3,12 +3,17 @@ package com.toptal.expenses.resources;
 import java.util.concurrent.atomic.AtomicLong;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
 import com.toptal.expenses.model.Saying;
 
+@Path("/hello-world")
+@Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource {
 
     private final String template;
